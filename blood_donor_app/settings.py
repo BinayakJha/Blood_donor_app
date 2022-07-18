9183bdeb632942ac14c07a1a478b753b3384ce16
Binayak Jha <jha36binayak@gmail.com>
2022-07-18 07:41:38 +0545

    Update settings.py

--- blood_donor_app/settings.py
+++ blood_donor_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_!ro_!=)hx6)uh+4^uk!^7(j#(3*dox3x5n_f&q-dvan_@yn$u'
+SECRET_KEY = 'SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
