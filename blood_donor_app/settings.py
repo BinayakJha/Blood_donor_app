886d4d4557c9c5caa73d3e486c3b13a696e6832b
Binayak Jha <jha36binayak@gmail.com>
2022-07-18 07:38:35 +0545

    Update settings.py

--- blood_donor_app/settings.py
+++ blood_donor_app/settings.py
@@ -122,8 +122,8 @@ EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='thegyane@gmail.com' #sender's email-id
-EMAIL_HOST_PASSWORD ='jdidjxhnufyoniqh' 
+EMAIL_HOST_USER ='YOUR EMAIL' #sender's email-id
+EMAIL_HOST_PASSWORD ='YOUR PASSWORD' 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
